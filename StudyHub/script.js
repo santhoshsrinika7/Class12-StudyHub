24b68dc480abceab95e6e99bc054334ec0b15a86
santhoshsrinika7 <edifyknp.srinika.007@gmail.com>
2026-09-08 16:51:08 +0530

    Add initial script.js for StudyHub with welcome message

---
+++ StudyHub/script.js
@@ -0,0 +1,8 @@
+// StudyHub JavaScript
+
+console.log("StudyHub loaded successfully");
+
+// Welcome message
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Welcome to Class 12 StudyHub!");
+});
